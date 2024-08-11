8c40c1ccb7967ddf18802bae99a06d4da1c825c2
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-08-10 20:38:36 -0500

    First commit. Nearly all of the back end is complete. Currently recieving responses in the browser.

---
+++ server/server.mjs
@@ -0,0 +1,78 @@
+import express from 'express';
+
+import { HNSWLib } from '@langchain/community/vectorstores/hnswlib';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
+import cors from 'cors';
+
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import {
+  RunnableLambda,
+  RunnableMap,
+  RunnablePassthrough,
+} from '@langchain/core/runnables';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const loadedVectorStore = await HNSWLib.load(
+  'data', new OpenAIEmbeddings());
+
+const retriever = loadedVectorStore.asRetriever(1);
+
+app.get('/', async (request, response) => {
+  const prompt = ChatPromptTemplate.fromMessages([
+    [
+      'ai',
+      'Answer the question from a cats perspective based ' +
+      'on only the following context:\n\n' +
+      '{context}'
+    ],
+    [
+      'ai',
+      'You are an cat that ' +
+      'answers questions for humans.' +
+      'Responses should include ' +
+      'answers from a cats perspective ' +
+      'and incorporate some cat personality.' +
+      'Each type of cat should have unique responses.' +
+      'Remember to keept it light and fun.' +
+      'Please do not say anything that could be offensive.'
+    ],
+    ['human', '{question}']
+  ]);
+
+  const setupAndRetrieval = RunnableMap.from({
+    context: new RunnableLambda({
+      func: (input) =>
+        retriever.invoke(input).then(
+          (response) => response[0].pageContent),
+    }).withConfig({ runName: 'contextRetriever' }),
+    question: new RunnablePassthrough(),
+  });
+
+  const model = new ChatOpenAI({});
+  const outputParser = new StringOutputParser();
+
+  const chain = setupAndRetrieval.pipe(prompt)
+    .pipe(model)
+    .pipe(outputParser);
+  
+  const stream = await chain.stream(
+    'From the prospective of a siamese cat can you tell me' +
+    'how to bake a cake?');
+
+  for await (const chunk of stream) {
+    if (chunk) {
+      response.write(chunk);
+    }
+  }
+
+  response.end();
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
