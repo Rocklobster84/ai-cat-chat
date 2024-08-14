e78f5901e2a1c50aa75b3b7dc33dba7713dc7c6b
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-08-14 10:13:11 -0500

    Made adjustments to font sizes in intro component for mobile.

--- client/src/components/Intro/Intro.jsx
+++ client/src/components/Intro/Intro.jsx
@@ -4,9 +4,9 @@ import './Intro.css';
 const Intro = () => {
   return(
     <>
-      <h1>AI Cat Chat</h1>
-        <h3>Welcome to Cat Chat. Your friendly AI cat ready to answer all of your questions.</h3>
-          <p>Once you choose a type of cat the responses given will be from the perspective of that type of cat.</p>
+      <h1 className="intro-head">AI Cat Chat</h1>
+        <h3 className="intro-sub">Welcome to Cat Chat. Your friendly AI cat ready to answer all of your questions.</h3>
+          <p className="intro-para">Once you choose a cat the responses given will be from the perspective of that type of cat.</p>
     </>
   )
 }
